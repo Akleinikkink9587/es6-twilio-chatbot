4859e0a5a4b950e000a18010436950d02b8d7ea0
akleinikkink9587 <akleinikkink9587@conetogac.on.ca>
2019-10-13 19:23:48 -0400

    continued story

--- Game.js
+++ Game.js
@@ -43,7 +43,6 @@ export default class Game {
                 if (sInput.toLowerCase().match("approach")) {
                     sReply = "You decide to try and approach the kid. He runs. Do you chase or continue walking?";
                     this.stateCur = GameState.CHASE;
-
                 } else {
                     sReply = "You decided to continue on your way. While walking you missed your corner and you are now lost. Do you call for help or continue walking?";
                     this.stateCur = GameState.LOST;
@@ -54,17 +53,16 @@ export default class Game {
                     sReply = "You decided to call for help. You took your phone our of your pocket and you have no signal. You see the same scrawny kid from the alley in the distance. Do you approach the kid or keep wandering till you find your way?";
                     this.stateCur = GameState.APPROACH_KID_LOST;
                 } else {
-                    sReply = "You decided to keep wandering. After 30 minutes of wandering you still have no idea where you are. You see the scrawny kid in the distance. Do you approach the kid or continue wondering?";
-                    this.stateCur = GameState.APPROACH_KID_LOST;
+                    sReply = "You decided to keep wandering. After 30 minutes of wandering you still have no idea where you are. Do you call for help or continue walking?";
+                    this.stateCur = GameState.LOST;
                 }
                 break;
             case GameState.APPROACH_KID_LOST:
                     if (sInput.toLowerCase().match("approach")) {
                         sReply = "You decide to try and approach the kid. He runs. Do you chase or continue walking?";
                         this.stateCur = GameState.CHASE;
-    
                     } else {
-                        sReply = "You decide to continue wondering. After 30 minutes still have no idea where you are. Do you try and call for help or continue wondering?";
+                        sReply = "You decide to continue wondering. After 30 minutes you still have no idea where you are. Do you try and call for help again or continue wondering?";
                         this.stateCur = GameState.LOST;
                     }
                     break;
@@ -79,36 +77,36 @@ export default class Game {
                 break;
             case GameState.FIGHT:
                 if (sInput.toLowerCase().match("fight")) {
-                    sReply = "You decide to try and fight off the kids to no avail. You lose the other kids from the alley and then watch the scwrany kid enter an old decrepit building. Do you enter the building or turn back?";
+                    sReply = "You decide to try and fight off the kids to no avail and have to run. You lose the other kids from the alley and then watch the scwrany kid enter an old decrepit building. Do you enter the building or turn back?";
                     this.stateCur = GameState.ENTER_HOUSE;
                 } else {
                     sReply = "You decide to chase after the scrawny kid. You lose the other kids from the alley and then watch the scwrany kid enter an old decrepit building. Do you enter the building or turn back?";
                     this.stateCur = GameState.ENTER_HOUSE;
                 }
                 break;
-            case GameState.ENTER_HOUSE://TODO:
+                case GameState.ENTER_HOUSE:
                 if (sInput.toLowerCase().match("enter")) {
-                    sReply = "You enter decrepit house and see the kid crying. ?";
-                    this.stateCur = GameState.FLAT;
+                    sReply = "You enter the decrepit house and see the kid crying. Do you comfort the kid or search the house?";
+                    this.stateCur = GameState.COMFORT_KID; //TODO: spelling
                 } else {
-                    sReply = "You seem to have walked in to a party. The host offers you some toast. Do you take the toast or ask to call a tow truck?";
-                    this.stateCur = GameState.TOAST;
+                    sReply = "You deicide to turn back but as you get look behind you the kids with weapons are searching for you in the distance. You have to enter the house.to escape from the kids. You see the scrawny kid crying. Do you comfort the kid or search the house? ";
+                    this.stateCur = GameState.COMFORT_KID;
                 }
                 break;
-            case GameState.APPROACH_KID:
+            case GameState.COMFORT_KID:
                 if (sInput.toLowerCase().match("run")) {
-                    sReply = "You decide to try and approach the kid. He runs. Do you chase or continue walking?";
-                    this.stateCur = GameState.FLAT;
+                    sReply = "You decide to try and comfort the kid. As you are comforting the kid he decides try and give you a hug. Do you push the kid away or let him give you a hug?";
+                    this.stateCur = GameState.COMFORT_HUG;
 
                 } else {
-                    sReply = "You seem to have walked in to a party. The host offers you some toast. Do you take the toast or ask to call a tow truck?";
-                    this.stateCur = GameState.TOAST;
+                    sReply = "You decide to search the house. After 15 minutes of searching you don't find aything useful. Do you comfort the kid or continue to search the house?";
+                    this.stateCur = GameState.COMFORT_KID;
 
                 }
                 break;
-            case GameState.APPROACH_KID:
+            case GameState.COMFORT_HUG: //TODO
                 if (sInput.toLowerCase().match("run")) {
-                    sReply = "You decide to try and approach the kid. He runs. Do you chase or continue walking?";
+                    sReply = "You ";
                     this.stateCur = GameState.FLAT;
 
                 } else {
